37bbbe813e070adc5962adb5dca50eb998f6e61a
Hector Zambrano Serrano <hectorzs@meta.com>
2025-06-04 17:25:23 -0700

    Day 2 project

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,21 @@
+// MODAL FUNCIONALITIES
+const modal = document.getElementById("song-modal");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(festival) {
+   document.getElementById('festivalName').innerText = festival.name;
+   document.getElementById('festivalImage').src = festival.imageUrl;
+   document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
+   document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
+   document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
